8d1105f9584cb69d0e09c89f0650c36ae97666a4
Yuichi Tateno <hotchpotch@gmail.com>
2015-10-12 19:19:36 +0900

    Refactoring

--- SN74141.cpp
+++ SN74141.cpp
@@ -5,25 +5,22 @@
 static uint8_t SN74141_FN_TABLE[][4] =
 {
   // D, C, B, A
-  { LOW  , LOW  , LOW  , LOW }  , // 0
+  { LOW  , LOW  , LOW  , LOW  } , // 0
   { LOW  , LOW  , LOW  , HIGH } , // 1
-  { LOW  , LOW  , HIGH , LOW }  , // 2
+  { LOW  , LOW  , HIGH , LOW  } , // 2
   { LOW  , LOW  , HIGH , HIGH } , // 3
-  { LOW  , HIGH , LOW  , LOW }  , // 4
+  { LOW  , HIGH , LOW  , LOW  } , // 4
   { LOW  , HIGH , LOW  , HIGH } , // 5
-  { LOW  , HIGH , HIGH , LOW }  , // 6
+  { LOW  , HIGH , HIGH , LOW  } , // 6
   { LOW  , HIGH , HIGH , HIGH } , // 7
-  { HIGH , LOW  , LOW  , LOW }  , // 8
+  { HIGH , LOW  , LOW  , LOW  } , // 8
   { HIGH , LOW  , LOW  , HIGH }   // 9
 };
 
 
 SN74141::SN74141(uint8_t a_pin, uint8_t b_pin ,uint8_t c_pin ,uint8_t d_pin)
 {
-  _a_pin = a_pin;
-  _b_pin = b_pin;
-  _c_pin = c_pin;
-  _d_pin = d_pin;
+  _a_pin = a_pin, _b_pin = b_pin, _c_pin = c_pin, _d_pin = d_pin;
 }
 
 void SN74141::begin()
@@ -36,7 +33,7 @@ void SN74141::begin()
 
 bool SN74141::outputPin(uint8_t out_pin)
 {
-  if (out_pin < 10) {
+  if (out_pin <= SN74141_OUTPUT_PIN_MAX) {
     digitalWrite(_d_pin, SN74141_FN_TABLE[out_pin][0]);
     digitalWrite(_c_pin, SN74141_FN_TABLE[out_pin][1]);
     digitalWrite(_b_pin, SN74141_FN_TABLE[out_pin][2]);
--- SN74141.h
+++ SN74141.h
@@ -2,13 +2,12 @@
 #ifndef __SN74141_H__
 #define __SN74141_H__
 
+#define SN74141_OUTPUT_PIN_MAX 9
+
 class SN74141
 {
 private:
-  uint8_t _a_pin;
-  uint8_t _b_pin;
-  uint8_t _c_pin;
-  uint8_t _d_pin;
+  uint8_t _a_pin, _b_pin, _c_pin, _d_pin;
 public:
   SN74141(uint8_t a_pin, uint8_t b_pin ,uint8_t c_pin ,uint8_t d_pin);
   void begin();
